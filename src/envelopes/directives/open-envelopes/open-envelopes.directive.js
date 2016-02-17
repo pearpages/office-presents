d6a1b9f8d1478fb02d82cc8ecef0f3909fdab360
pearpages <pere@soms.cat>
2016-02-17 11:06:31 +0100

    open-envelopes directive

---
+++ src/envelopes/directives/open-envelopes/open-envelopes.directive.js
@@ -0,0 +1,45 @@
+(function() {
+	'use strict';
+
+	angular.module("myEnvelopes")
+	.directive('openEnvelopes',['envelopes','NgTableParams','currentUser',openEnvelopes]);
+
+	function openEnvelopes(envelopes,NgTableParams,currentUser) {
+		return {
+				restrict: 'E',
+				bindToController: true,
+				controllerAs: 'vmd',
+				controller: controller,
+				scope:{
+					
+				},
+				templateUrl: 'src/envelopes/directives/open-envelopes/partials/open-envelopes.html',
+			};
+		
+		
+			function controller() {
+				var vmd = this;
+		
+		        vmd.envelopes;
+		        vmd.user;        
+		        vmd.addContributor = addContributor;
+		        vmd.removeContributor = removeContributor;
+
+		        activate();
+
+		        function activate() {
+		        	vmd.user = currentUser;	
+		        	var open = envelopes.getOpenEnvelopes(vmd.user.get());
+		        	vmd.envelopes = new NgTableParams({page:1,count: 10},{data: open});
+		        } 
+
+		        function addContributor(envelope,user) {
+		        	envelope.addContributor(currentUser.get());
+		        }
+
+		        function removeContributor(envelope,user) {
+		        	envelope.removeContributor(currentUser.get());
+		        }
+			}
+	}
+})();
--- src/home/Home.controller.js
+++ src/home/Home.controller.js
@@ -2,32 +2,12 @@
     'use strict';
 
     angular.module("office-presents")
-    .controller('HomeController',['envelopes','NgTableParams','currentUser',HomeController]);
+    .controller('HomeController',[HomeController]);
 
-    function HomeController(envelopes,NgTableParams,currentUser) {
+    function HomeController() {
 
         var vm = this;
 
-        vm.envelopes;
-        vm.user;        
-        vm.addContributor = addContributor;
-        vm.removeContributor = removeContributor;
-
-        activate();
-
-        function activate() {
-        	vm.user = currentUser;	
-        	vm.envelopes = new NgTableParams({page:1,count: envelopes.getOpenEnvelopes(vm.user.get()).length},{data: envelopes.getOpenEnvelopes(vm.user.get())});
-        } 
-
-        function addContributor(envelope,user) {
-        	envelope.addContributor(currentUser.get());
-        }
-
-        function removeContributor(envelope,user) {
-        	envelope.removeContributor(currentUser.get());
-        }
-        
     }
 
 })();
