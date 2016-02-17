6aad86ba9fbe15bbe4916022906925c719ebce54
pearpages <pere@soms.cat>
2016-02-17 12:56:39 +0100

    assign envelope

--- src/envelopes/envelope/envelope.controller.js
+++ src/envelopes/envelope/envelope.controller.js
@@ -2,21 +2,27 @@
 	'use strict';
 
 	angular.module("myEnvelopes")
-	.controller('EnvelopeController',['$state','envelopes','currentUser',EnvelopeController]);
+	.controller('EnvelopeController',['$state','envelopes','currentUser','users',EnvelopeController]);
 
-	function EnvelopeController($state,envelopes,currentUser) {
+	function EnvelopeController($state,envelopes,currentUser,users) {
 
 		var vm = this;
 
 		vm.envelope;
+		vm.responsible;
 		vm.show = show;
 		vm.contribute = contribute;
 		vm.removeContibution = removeContibution;
+		vm.users = users;
+		vm.assign = assign;
 
 		activate();
 
 		function activate() {
 			vm.envelope = envelopes.getEnvelope($state.params.id);
+			if(vm.envelope.responsible) {
+				vm.responsible = vm.envelope.responsible.id;
+			}
 		}
 
 		function show() {
@@ -35,6 +41,12 @@
 
 		function removeContibution() {
 			vm.envelope.removeContributor(currentUser.get());
+		}
+
+		function assign() {
+			var assigned = users.getUser(vm.responsible);
+			vm.envelope.responsible = assigned;
+			assigned.responsible.push(vm.envelope);
 		}
 	}
 })();
